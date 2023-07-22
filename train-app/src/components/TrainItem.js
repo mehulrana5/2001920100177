a597b7182e5e8e45cf3edf7e1952c30741823a09
Mehul <cs20077@glbitm.ac.in>
2023-07-22 20:49:01 +0530

    2

---
+++ train-app/src/components/TrainItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function TrainItem() {
+    return (
+        <div className='container'>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Train name</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">(Train number)</h6>
+                    <p class="card-text"></p>
+                    <a href="/" class="card-link">Card link</a>
+                    <a href="/" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+    )
+}
